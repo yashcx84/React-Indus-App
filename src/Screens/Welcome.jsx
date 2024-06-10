ab2245bd078224b4eb34f8cc4c4ed7fdc9577175
Yash Jangid <yashcx84@gmail.com>
2024-06-10 11:44:04 +0530

    /Imports Modified.

--- src/Screens/Welcome.jsx
+++ src/Screens/Welcome.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Container, Row, Col, Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import handslock from '../assets/Images/handslock.jpg';
 import slide1 from '../assets/Images/home-banner.jpg';
 import slide2 from '../assets/Images/home-banner2.jpg';
 import slide3 from '../assets/Images/home-banner3.jpg';
